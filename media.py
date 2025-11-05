5278efa8ab5a4c41d8b625e09d247db918cbbe89
pedro <pedrpphilipe2019@gmail.com>
2025-11-05 11:07:37 -0300

    commit

--- media.py
+++ media.py
@@ -2,4 +2,9 @@ nomeAluno = input ("Nome do Aluno")
 nota1 = float(input("Primeira Nota "))
 nota2 = float(input("Segunda nota"))
 media = (nota1 + nota2)/2
-print ("a nota do aluno é: (media) ")
+print ("A média do aluno", nomeAluno, "é", media)
+aprovado = media >=7
+if media >=7:
+    print ("O aluno está aprovado")
+else:
+    print ("O aluno está reprovado")
